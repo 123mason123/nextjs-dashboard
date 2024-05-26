1a95a221b6ad85804cde5499119e61d6407ebe61
123mason123 <130639689+123mason123@users.noreply.github.com>
2024-05-26 16:02:19 +0800

    ee
    ee

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
@@ -7,6 +8,12 @@ import {
   fetchLatestInvoices,
   fetchCardData,
 } from '@/app/lib/data';
+
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+  CardsSkeleton,
+} from '@/app/ui/skeletons';
  
 export default async function Page() {
   const revenue = await fetchRevenue();
